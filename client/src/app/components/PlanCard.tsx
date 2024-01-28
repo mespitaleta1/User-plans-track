5d3e8c5139b2e974e35fb4da9a2a025233eb94a1
melissa espitaleta <m.espitaleta1@gmail.com>
2024-01-28 22:05:28 +0100

    feat: adding the initial plansCard structure component

---
+++ client/src/app/components/PlanCard.tsx
@@ -0,0 +1,59 @@
+import {STATUS} from "../constans/constans";
+
+export interface PlanCardProps {
+    status: string;
+    dateStart: string; 
+    dateEnd: null | string;
+    comsuption: null |{
+        totalComsuption: number;
+    };
+    flag: string;
+    country: string;
+    plan: string;
+};
+
+const PlanCard = ({
+    status,
+    dateStart,
+    dateEnd,
+    comsuption,
+    flag,
+    country,
+    plan,
+    }: PlanCardProps) => {
+  
+  
+      const statusClasses = {
+          [STATUS.PENDING]: {bg: "bg-amber-500/10", text:"text-amber-500" },
+          [STATUS.ACTIVE]: {bg:"bg-sky-500/10", text:"text-blue-900"},
+          [STATUS.EXPIRED]: {bg:"bg-gray-400/10", text:"text-gray-700", flag: "grayscale"}
+      }
+      const fontOnExpiredClass = status === STATUS.EXPIRED ? "text-xs text-gray-400" : "text-gray-600 text-sm";
+      
+      return (
+        <div className=" px-8 py-8">
+          {/*Flag and status card info */}
+          <div className={`w-fit flex items-center flex-wrap rounded-full ${statusClasses[status].bg}`}>
+            <div className="flex items-center justify-center h-6 w-6 rounded-full bg-white">
+            <img
+              className={`inline-block h-5 w-5 rounded-full ${statusClasses[status].flag}`}
+              src={flag}
+              alt={`${country} flag`}                
+            />
+            </div>
+    
+            <p className={`${statusClasses[status].text} text-xs pl-2 pr-3`}>{status}</p>
+          </div>
+  
+         {/* Content Wrapper */}
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900 text-left">{country}</h2>
+            {status === STATUS.EXPIRED && <p className="mt-1 flex items-baseline text-gray-600 text-xs">{`${dateStart} - ${dateEnd}`}</p>}
+            <p className={`mt-1 flex items-baseline ${fontOnExpiredClass}`}>{plan}</p>
+            
+          </div>
+        </div>
+      );
+  }
+  
+  export default PlanCard;
